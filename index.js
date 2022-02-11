2a6b6005a0cc8b9746417af59a6614f707a53164
Alisa Gezalova <alisa.gezalova@gmail.com>
2022-02-10 21:08:15 -0700

    commit tree 3ce625369965e89793118f110f1038eb3d4b8959

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Awesome Person",
+    streetAddress: "Mega Cool Street",
+}
+
+
+
+function updateEmployeeWithKeyAndValue(employee, hobby, hobbyValue) {
+    const newEmployee = { ...employee};
+    newEmployee[hobby] = hobbyValue;
+    return newEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, favoriteColor, favoriteColorValue) {
+    employee[favoriteColor] = favoriteColorValue;
+    return employee;
+}
+
+function deleteFromEmployeeByKey() {
+    const newEmployee2 = { ...employee}
+    delete newEmployee2.name;
+    return newEmployee2;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    return employee
+}
